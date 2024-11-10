e4bebb2b581c3a72c7e46ca0322f38c2938fbfd0
Austin Owensby <austin_owensby@hotmail.com>
2024-11-10 00:47:15 -0600

    Regenerate service files

--- Services/2024/Solution2024_01Service.cs
+++ Services/2024/Solution2024_01Service.cs
@@ -1,9 +1,9 @@
 namespace EverybodyCodes.Services
 {
-    // (ctrl/command + click) the link to open the input file
-    // file://./../../Inputs/2024/01.txt
     public class Solution2024_01Service : ISolutionQuestService
     {
+        // (ctrl/command + click) the link to open the input file
+        // file://./../../Inputs/2024/01_part1.txt
         public string PartOne(bool example)
         {
             List<string> lines = Utility.GetInputLines(2024, 1, 1, example);
@@ -14,10 +14,12 @@ namespace EverybodyCodes.Services
             {
 
             }
-            
+
             return answer.ToString();
         }
 
+        // (ctrl/command + click) the link to open the input file
+        // file://./../../Inputs/2024/01_part2.txt
         public string PartTwo(bool example)
         {
             List<string> lines = Utility.GetInputLines(2024, 1, 2, example);
@@ -28,10 +30,12 @@ namespace EverybodyCodes.Services
             {
 
             }
-            
+
             return answer.ToString();
         }
 
+        // (ctrl/command + click) the link to open the input file
+        // file://./../../Inputs/2024/01_part3.txt
         public string PartThree(bool example)
         {
             List<string> lines = Utility.GetInputLines(2024, 1, 3, example);
