5301ae5fdcc39a657a1b308b7e13768fa27d9b6d
Austin Owensby <austin_owensby@hotmail.com>
2024-11-12 22:53:58 -0600

    Day 3

--- Services/2024/Solution2024_03Service.cs
+++ Services/2024/Solution2024_03Service.cs
@@ -7,12 +7,45 @@ namespace EverybodyCodes.Services
         public string PartOne(bool example)
         {
             List<string> lines = Utility.GetInputLines(2024, 3, 1, example);
+            List<List<char>> grid = lines.Select(x => x.ToList()).ToList();
+
+            int height = grid.Count;
+            int width = grid.First().Count;
 
             int answer = 0;
 
-            foreach (string line in lines)
-            {
+            List<int> layerCounts = [];
 
+            // Loop until all the spots are dug
+            while (grid.Any(row => row.Any(cell => cell == '#')))
+            {
+                List<List<char>> newGrid = grid.Select(r => r.ToList()).ToList();
+                int layerCount = 0;
+                // Loop over each row
+                foreach (int y in height)
+                {
+                    // Loop over each column
+                    foreach (int x in width)
+                    {
+                        // Check if this cell is approved to dig at
+                        if (grid[y][x] == '#') {
+                            // Check if there is an adjacent empty neighbor to maintain a safe slope
+                            List<Point> neighbors = grid.GetNeighbors(x, y);
+
+                            if (neighbors.Any(n => grid[n.Y][n.X] == '.')) {
+                                layerCount++;
+                                newGrid[y][x] = '.';
+                            }
+                        }
+                    }
+                }
+            
+                grid = newGrid;
+                layerCounts.Add(layerCount);
+            }
+
+            foreach (int i in layerCounts.Count) {
+                answer += (i + 1) * layerCounts[i];
             }
 
             return answer.ToString();
@@ -23,12 +56,45 @@ namespace EverybodyCodes.Services
         public string PartTwo(bool example)
         {
             List<string> lines = Utility.GetInputLines(2024, 3, 2, example);
+            List<List<char>> grid = lines.Select(x => x.ToList()).ToList();
+
+            int height = grid.Count;
+            int width = grid.First().Count;
 
             int answer = 0;
 
-            foreach (string line in lines)
-            {
+            List<int> layerCounts = [];
 
+            // Loop until all the spots are dug
+            while (grid.Any(row => row.Any(cell => cell == '#')))
+            {
+                List<List<char>> newGrid = grid.Select(r => r.ToList()).ToList();
+                int layerCount = 0;
+                // Loop over each row
+                foreach (int y in height)
+                {
+                    // Loop over each column
+                    foreach (int x in width)
+                    {
+                        // Check if this cell is approved to dig at
+                        if (grid[y][x] == '#') {
+                            // Check if there is an adjacent empty neighbor to maintain a safe slope
+                            List<Point> neighbors = grid.GetNeighbors(x, y);
+
+                            if (neighbors.Any(n => grid[n.Y][n.X] == '.')) {
+                                layerCount++;
+                                newGrid[y][x] = '.';
+                            }
+                        }
+                    }
+                }
+            
+                grid = newGrid;
+                layerCounts.Add(layerCount);
+            }
+
+            foreach (int i in layerCounts.Count) {
+                answer += (i + 1) * layerCounts[i];
             }
 
             return answer.ToString();
@@ -39,12 +105,46 @@ namespace EverybodyCodes.Services
         public string PartThree(bool example)
         {
             List<string> lines = Utility.GetInputLines(2024, 3, 3, example);
+            List<List<char>> grid = lines.Select(x => x.ToList()).ToList();
+
+            int height = grid.Count;
+            int width = grid.First().Count;
 
             int answer = 0;
 
-            foreach (string line in lines)
-            {
+            List<int> layerCounts = [];
 
+            // Loop until all the spots are dug
+            while (grid.Any(row => row.Any(cell => cell == '#')))
+            {
+                List<List<char>> newGrid = grid.Select(r => r.ToList()).ToList();
+                int layerCount = 0;
+                // Loop over each row
+                foreach (int y in height)
+                {
+                    // Loop over each column
+                    foreach (int x in width)
+                    {
+                        // Check if this cell is approved to dig at
+                        if (grid[y][x] == '#') {
+                            // Check if there is an adjacent empty neighbor to maintain a safe slope
+                            List<Point> neighbors = grid.GetNeighbors(x, y, true);
+
+                            // If neighbors is < 8, then we're at a border, assume it's good to dig here
+                            if (neighbors.Count < 8 || neighbors.Any(n => grid[n.Y][n.X] == '.')) {
+                                layerCount++;
+                                newGrid[y][x] = '.';
+                            }
+                        }
+                    }
+                }
+            
+                grid = newGrid;
+                layerCounts.Add(layerCount);
+            }
+
+            foreach (int i in layerCounts.Count) {
+                answer += (i + 1) * layerCounts[i];
             }
 
             return answer.ToString();
