b12e3348701a5f5ce97d8191b035e81ea0af463f
Austin Owensby <austin_owensby@hotmail.com>
2024-11-19 21:21:29 -0600

    Quest 8

--- Services/2024/Solution2024_08Service.cs
+++ Services/2024/Solution2024_08Service.cs
@@ -8,13 +8,15 @@ namespace EverybodyCodes.Services
         {
             List<string> lines = Utility.GetInputLines(2024, 8, 1, example);
 
-            int answer = 0;
+            int blocks = int.Parse(lines.First());
 
-            foreach (string line in lines)
-            {
+            int step = (int)Math.Ceiling(Math.Sqrt(blocks));
+            int targetWidth = 2 * (step - 1) + 1;
+            int targetBlocks = step * step;
+            int missingBlocks = targetBlocks - blocks;
 
-            }
-
+            int answer = missingBlocks * targetWidth;
+            
             return answer.ToString();
         }
 
@@ -24,12 +26,25 @@ namespace EverybodyCodes.Services
         {
             List<string> lines = Utility.GetInputLines(2024, 8, 2, example);
 
-            int answer = 0;
+            int priests = int.Parse(lines.First());
+            int acolytes = example ? 5 : 1111;
+            int supply = example ? 50 : 20240000;
 
-            foreach (string line in lines)
-            {
+            int usedBlocks = 0;
+            int thickness = 1;
+            int width = 1;
 
+            while (usedBlocks <= supply) {
+                usedBlocks += width * thickness;
+                thickness = thickness * priests % acolytes;
+                width += 2;
             }
+
+            width -= 2;
+
+            int blocksNeeded = usedBlocks - supply;
+
+            int answer = blocksNeeded * width;
 
             return answer.ToString();
         }
@@ -40,12 +55,31 @@ namespace EverybodyCodes.Services
         {
             List<string> lines = Utility.GetInputLines(2024, 8, 3, example);
 
-            int answer = 0;
+            long priests = int.Parse(lines.First());
+            long acolytes = example ? 5 : 10;
+            long supply = example ? 160 : 202400000;
 
-            foreach (string line in lines)
-            {
+            long usedBlocks = 0;
+            long thickness = 1;
+            long width = 1;
 
+            List<long> thicknessHistory = [thickness];
+
+            while (usedBlocks <= supply) {
+                usedBlocks = thickness * (width == 1 ? 1 : 2);
+
+                for (int i = 1; i <= (width - 1) / 2; i++) {
+                    long columnHeight = thicknessHistory.TakeLast(i + 1).Sum();
+                    long emptySpace = priests * width * columnHeight % acolytes;
+                    usedBlocks += (i == (width - 1) / 2 ? 1 : 2) * (columnHeight - emptySpace);
+                }
+
+                thickness = thickness * priests % acolytes + acolytes;
+                width += 2;
+                thicknessHistory.Add(thickness);
             }
+
+            long answer = usedBlocks - supply;
 
             return answer.ToString();
         }
