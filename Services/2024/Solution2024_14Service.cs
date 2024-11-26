edb28f9f6a30a4f5ae714d4760cb8248f91d39c9
Austin Owensby <austin_owensby@hotmail.com>
2024-11-25 19:20:01 -0600

    Quest 14 part 2

--- Services/2024/Solution2024_14Service.cs
+++ Services/2024/Solution2024_14Service.cs
@@ -39,10 +39,31 @@ namespace EverybodyCodes.Services
             {
                 Point3D currentPoint = new(0, 0, 0);
 
-            foreach (string line in lines)
-            {
+                foreach ((char, int) direction in directionSet)
+                {
+                    foreach (int i in direction.Item2)
+                    {
+                        Point3D nextPoint = direction.Item1 switch
+                        {
+                            'U' => new(currentPoint.X, currentPoint.Y + 1, currentPoint.Z),
+                            'D' => new(currentPoint.X, currentPoint.Y - 1, currentPoint.Z),
+                            'L' => new(currentPoint.X - 1, currentPoint.Y, currentPoint.Z),
+                            'R' => new(currentPoint.X + 1, currentPoint.Y, currentPoint.Z),
+                            'F' => new(currentPoint.X, currentPoint.Y, currentPoint.Z + 1),
+                            'B' => new(currentPoint.X, currentPoint.Y, currentPoint.Z - 1),
+                            _ => throw new NotImplementedException()
+                        };
 
+                        currentPoint = nextPoint;
+
+                        if (!points.Any(p => p.X == nextPoint.X && p.Y == nextPoint.Y && p.Z == nextPoint.Z)) {
+                            points.Add(nextPoint);
+                        }
+                    }
+                }
             }
+
+            int answer = points.Count;
 
             return answer.ToString();
         }
@@ -53,13 +74,150 @@ namespace EverybodyCodes.Services
         {
             List<string> lines = Utility.GetInputLines(2024, 14, 3, example);
 
-            int answer = 0;
+            List<List<(char, int)>> directionSets = lines.Select(line => line.Split(',').Select(x => (x[0], int.Parse(x.Substring(1, x.Length - 1)))).ToList()).ToList();
 
-            foreach (string line in lines)
+            List<Point3D> points = [];
+            List<Point3D> leaves = [];
+
+            // Get the list of all points and leaves
+            foreach (List<(char, int)> directionSet in directionSets)
             {
+                Point3D currentPoint = new(0, 0, 0);
 
+                foreach ((char, int) direction in directionSet)
+                {
+                    foreach (int i in direction.Item2)
+                    {
+                        Point3D nextPoint = direction.Item1 switch
+                        {
+                            'U' => new(currentPoint.X, currentPoint.Y + 1, currentPoint.Z),
+                            'D' => new(currentPoint.X, currentPoint.Y - 1, currentPoint.Z),
+                            'L' => new(currentPoint.X - 1, currentPoint.Y, currentPoint.Z),
+                            'R' => new(currentPoint.X + 1, currentPoint.Y, currentPoint.Z),
+                            'F' => new(currentPoint.X, currentPoint.Y, currentPoint.Z + 1),
+                            'B' => new(currentPoint.X, currentPoint.Y, currentPoint.Z - 1),
+                            _ => throw new NotImplementedException()
+                        };
+
+                        currentPoint = nextPoint;
+
+                        if (!points.Any(p => p.X == nextPoint.X && p.Y == nextPoint.Y && p.Z == nextPoint.Z)) {
+                            points.Add(nextPoint);
+                        }
+                    }
+                }
+            
+                leaves.Add(currentPoint);
             }
 
+            Dictionary<string, List<(int, int)>> leafToTrunkBest = [];
+
+            // 1304
+            // 1320
+
+            foreach (Point3D leaf in leaves)
+            {
+                Dictionary<string, int> distances = [];
+                int bestDistanceToTrunk = int.MaxValue;
+
+                Queue<Point3D> queue = [];
+                queue.Enqueue(leaf);
+
+                while (queue.Count > 0)
+                {
+                    Point3D point = queue.Dequeue();
+
+                    // Record best distance
+                    string key = $"{point.X},{point.Y},{point.Z}";
+                    if (distances.TryGetValue(key, out int value)) {
+                        if (value > point.Value) {
+                            distances[key] = point.Value;
+                        }
+                        else {
+                            // This point is not better, ignore it
+                            continue;
+                        }
+                    }
+                    else {
+                        distances[key] = point.Value;
+                    }
+
+                    // We found a better answer
+                    if (point.X == 0 && point.Z == 0) {
+                        if (bestDistanceToTrunk > point.Value) {
+                            bestDistanceToTrunk = point.Value;
+                            leafToTrunkBest[$"{leaf.X},{leaf.Y},{leaf.Z}"] = [(point.Y, point.Value)];
+                        }
+                        else if (bestDistanceToTrunk == point.Value) {
+                            bestDistanceToTrunk = point.Value;
+                            leafToTrunkBest[$"{leaf.X},{leaf.Y},{leaf.Z}"].Add((point.Y, point.Value));
+                        }
+                        continue;
+                    }
+
+                    // We're worse than our best, ignore this point
+                    if (point.Value > bestDistanceToTrunk)
+                    {
+                        continue;
+                    }
+
+                    // Check neighbors
+                    if (points.Any(p => p.X == point.X + 1 && p.Y == point.Y && p.Z == point.Z)) {
+                        Point3D newPoint = new(point.X + 1, point.Y, point.Z){
+                            Value = point.Value + 1
+                        };
+                        queue.Enqueue(newPoint);
+                    }
+                    if (points.Any(p => p.X == point.X - 1 && p.Y == point.Y && p.Z == point.Z)) {
+                        Point3D newPoint = new(point.X - 1, point.Y, point.Z){
+                            Value = point.Value + 1
+                        };
+                        queue.Enqueue(newPoint);
+                    }
+                    if (points.Any(p => p.X == point.X && p.Y == point.Y + 1 && p.Z == point.Z)) {
+                        Point3D newPoint = new(point.X, point.Y + 1, point.Z){
+                            Value = point.Value + 1
+                        };
+                        queue.Enqueue(newPoint);
+                    }
+                    if (points.Any(p => p.X == point.X && p.Y == point.Y - 1 && p.Z == point.Z)) {
+                        Point3D newPoint = new(point.X, point.Y - 1, point.Z){
+                            Value = point.Value + 1
+                        };
+                        queue.Enqueue(newPoint);
+                    }
+                    if (points.Any(p => p.X == point.X && p.Y == point.Y && p.Z == point.Z + 1)) {
+                        Point3D newPoint = new(point.X, point.Y, point.Z + 1){
+                            Value = point.Value + 1
+                        };
+                        queue.Enqueue(newPoint);
+                    }
+                    if (points.Any(p => p.X == point.X && p.Y == point.Y && p.Z == point.Z - 1)) {
+                        Point3D newPoint = new(point.X, point.Y, point.Z - 1){
+                            Value = point.Value + 1
+                        };
+                        queue.Enqueue(newPoint);
+                    }
+                }
+            }
+
+            int answer = int.MaxValue;
+
+            List<Point3D> trunk = points.Where(p => p.X == 0 && p.Z == 0).ToList();
+
+            foreach (Point3D trunkPoint in trunk) {
+                int murkinessLevel = 0;
+
+                foreach (List<(int, int)> leaf in leafToTrunkBest.Values) {
+                    int bestY = leaf.Select(l => Math.Abs(trunkPoint.Y - l.Item1)).Min();
+                    murkinessLevel += bestY + leaf[0].Item2;
+                }
+
+                if (murkinessLevel < answer) {
+                    answer = murkinessLevel;
+                }
+            }
+            
             return answer.ToString();
         }
     }
